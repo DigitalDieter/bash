78acebf74dbe5836eadcbc98fd3d1b9012b02a9c
Michael <m.mueller444@gmail.com>
2019-06-12 23:56:14 +0200

    Updated code

---
+++ create_ssh_keypair.sh
@@ -0,0 +1,9 @@
+#!/usr/bin bash
+
+echo "Generating  sshkeys (without password)"
+if [ ! -d /home/$(whoami)/.ssh ]
+then
+    mkdir -p /home/$(whoami)/.ssh
+    ssh-keygen -t ed25519 -o -a 100 -N "" -f /home/$(whoami)/.ssh/$(whoami).key
+fi
+
