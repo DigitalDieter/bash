f723d7139989707e0124ae172a748cae3883935a
Michael <m.mueller444@gmail.com>
2019-07-02 21:30:34 +0200

    create_adminuser.sh

---
+++ create_adminuser.sh
@@ -0,0 +1,8 @@
+
+read -p "Enter username: " USER
+echo "create user"
+sudo adduser --gecos "" $USER
+echo "add user to sudo group"
+usermod -aG sudo $USER
+echo "change user to newly created"
+su - $USER
