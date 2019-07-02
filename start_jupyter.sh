76f92e7cb4b75942844b9d9029a2a308823f7887
Michael <m.mueller444@gmail.com>
2019-07-03 01:05:59 +0200

    start_jupyter.sh updated

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,12 +1,5 @@
+#!/bin/bash
 
 # start jupyter notebook
 jupyter notebook --ip=0.0.0.0 --no-browser
 
-
-
-# Install libs for R support
-sudo apt-get install -y r-base r-base-dev libssl-dev libcurl3-dev curl
-
-
-export XDG_RUNTIME_DIR=""
-jupyter notebook --ip=0.0.0.0 --no-browser
