fbbf1769f550298059709e4c55c53a560857907e
Michael <m.mueller444@gmail.com>
2019-07-02 23:32:30 +0200

    install_anconda.sh updated

--- install_anconda.sh
+++ install_anconda.sh
@@ -13,12 +13,13 @@ if [ ! -d /home/anaconda/anaconda ]
 then
     sudo chown -R anaconda:anaconda /home/anaconda
     sudo chmod -R go-w /home/anaconda
-    echo "Create folder for installation"
     echo "Downloading anaconda script intaller"
     curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
     echo "Install anaconda"
-    installation_dir='/home/anaconda'
+    installation_dir='/home/anaconda/anaconda'
     sudo -u anaconda sudo bash Anaconda3-2019.03-Linux-x86_64.sh -b -f -p ${installation_dir}
+    echo "change user"
+    su - anaconda
     echo "Execute conda info"
     conda info
 fi
