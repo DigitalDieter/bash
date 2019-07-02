3261db3d4cc29598fb5947e7b2b903376d2046a8
Michael <m.mueller444@gmail.com>
2019-07-02 23:37:15 +0200

    start_jupyter.sh updated

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,22 +1,11 @@
 
-#Jupyter Hzub
-pip3 install jupyter
+# start jupyter notebook
+jupyter notebook --ip=0.0.0.0 --no-browser
 
 
-# Install libs for R support 
+
+# Install libs for R support
 sudo apt-get install -y r-base r-base-dev libssl-dev libcurl3-dev curl
-
-pip install --upgrade jupyter_enterprise_gateway+
-
-
-
-apt-get install unzip emacs wget curl python3-pip
-pip3 install -U jupyter
-
-#Start Kernel from terminal
-
-
-jupyter enterprisegateway --ip=0.0.0.0 --port_retries=0
 
 
 export XDG_RUNTIME_DIR=""
