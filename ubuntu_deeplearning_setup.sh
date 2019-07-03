84af1133e3bb38154180578ded392ddb639a0d49
Michael <m.mueller444@gmail.com>
2019-07-03 22:50:09 +0200

    ubuntu_deeplearning_setup.sh updated

--- ubuntu_deeplearning_setup.sh
+++ ubuntu_deeplearning_setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# #####################################
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -y build-essential cmake g++ gfortran git pkg-config python-dev software-properties-common wget
-sudo apt-get autoremove
-sudo rm -rf /var/lib/apt/lists/*
+sudo apt  update
+sudo apt -y upgrade
+sudo apt install -y build-essential cmake g++ gfortran git pkg-config python-dev software-properties-common wget
+sudo apt -y autoremove
 
 sudo adduser anaconda
 sudo usermod -aG sudo anaconda
@@ -14,33 +12,10 @@ ufw app list
 
 
 #################    ANACONDA  #################
-echo "Create user anaconda"
+#echo "Create user anaconda"
 
-sudo adduser anaconda --disabled-password --shell /bin/bash --gecos "User" $username
-adduser anaconda --disabled-password --shell /bin/bash --gecos "sudo" $username
-
-
-if [ ! -d /opt/anaconda ]
-then
-    echo "Create folder for installation"
-    mkdir -p /opt/anaconda
-    cd /opt/anaconda
-    echo "Downloading anaconda script intaller"
-    curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-    echo "Install anaconda"
-    installation_dir='/opt/anaconda'
-    bash Anaconda3-2019.03-Linux-x86_64.sh -b -f -p ${installation_dir}
-    echo "Change folder permission"
-    chown -R anaconda:anaconda /opt/anaconda
-    chmod -R go-w /opt/anaconda
-    chmod -R go+rX /opt/anaconda
-    #
-
-    su anaconda
-
-    conda info
-fi
-
+#sudo adduser anaconda --disabled-password --shell /bin/bash --gecos "User" $username
+#adduser anaconda --disabled-password --shell /bin/bash --gecos "sudo" $username
 
 
 
