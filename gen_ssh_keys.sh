11c09a339f9bb2c12d9ff24a1d2cb4893498a437
Michael <m.mueller444@gmail.com>
2019-07-03 22:41:03 +0200

    gen_ssh_keys.sh added

---
+++ gen_ssh_keys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Generate ssh keypair for current user
+
+echo "Generating  sshkeys (without password)"
+if [ ! -d /home/$(whoami)/.ssh ]
+then
+    mkdir -p /home/$(whoami)/.ssh
+    ssh-keygen -t ed25519 -o -a 100 -N "" -f /home/$(whoami)/.ssh/$(whoami).key
+fi
