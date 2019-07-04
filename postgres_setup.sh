fe149c8c1cd45dc9368390f54c18064d86dde743
Michael <m.mueller444@gmail.com>
2019-07-04 07:22:36 +0200

    postgres_setup.sh updated

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Download / Install postgressql 
+# Download / Install postgressql
 
-echo"Download and Setup a postgres db"
-# Install dependencies
-sudo apt install-y postgresql-client-common postgresql postgresql-contrib  postgresql-client-common
-# Create user and database
+EI="\e[1;32m"
+EO="\e[0m"
+
+
+desc="Installing postgres dependencies"
+echo -e $EI $desc $EO
+sudo apt install -y postgresql-client-common postgresql postgresql-contrib  postgresql-client-common
+
+
+desc="Creating postgres user & database"
+echo -e $EI $desc $EO
 sudo -u postgres createuser -s $(whoami); createdb $(whoami)
-# Test login
-# check if service is online
+
+desc="Checking if postgres service is running"
+echo -e $EI $desc $EO
 ss -nlt |grep 5432
-#sudo -u postgres psql
+
