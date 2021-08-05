5f3d4b51493318871219b91bb6081ecf86ec6114
Michael Mueller <m.mueller444@gmail.com>
2021-08-05 17:22:54 +0200

    Update gen_ssh_keys.sh
    - fixed wrong .ssh & Home path

--- gen_ssh_keys.sh
+++ gen_ssh_keys.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+GREEN="\033[01;32m"
+BLUE="\033[01;34m"
+RED="\033[01;31m"
+WHITEB="\033[01;37m"
+RESET="\033[00m"
 
-# Generate ssh keypair for current user
 
-EI="\e[1;32m"
-EO="\e[0m"
+if [[ "$(id -u)" -ne 0 ]]; then
+printf "\n${RED}[+]${RESET}${WHITEB} Please, run this program as root! ${RESET}\n"
+exit 1
+fi
 
-desc="Generating  sshkeys (without password)"
-echo -e $EI $desc $EO
-
-if [ ! -d /home/$(whoami)/.ssh ]
+printf "\n${BLUE}[+]${RESET}${WHITEB} Generating  sshkeys (without password) ${RESET}\n"
+if [[  ! -d /home/$(whoami)/.ssh ]]
 then
-    mkdir -p /home/$(whoami)/.ssh
-    ssh-keygen -t ed25519 -o -a 100 -N "" -f /home/$(whoami)/.ssh/$(whoami).key
+sudo mkdir -p $HOME/$(whoami)/.ssh
+sudo ssh-keygen -t ed25519 -o -a 100 -N "" -f $HOME/.ssh/$(whoami)
 fi
